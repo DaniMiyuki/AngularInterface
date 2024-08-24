111a3266eba6d669b80e23e55c6976e6920a9d0b
Kanayama Miyuki <miyukitiam@gmail.com>
2024-08-24 19:59:49 +0900

    Route and components configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from "./core/header/header.component";
+import { FooterComponent } from "./core/footer/footer.component";
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'miyuki-app';
+  title = 'App-component';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PatientComponent } from './pages/patient/patient.component';
+import { ScheduleComponent } from './pages/schedule/schedule.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'header', component: HeaderComponent},
+    { path: 'footer', component: FooterComponent},
+    { path: '', component: HomeComponent},
+    { path: 'home', component: HomeComponent},
+    { path: 'patient', component: PatientComponent},
+    { path: 'schedule', component: ScheduleComponent},
+    { path: 'settings', component: SettingsComponent},
+    { path: 'contact', component: ContactComponent},
+];
