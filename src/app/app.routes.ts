f664a89448c00e9692f88994de01d87df300400b
Kanayama Miyuki <miyukitiam@gmail.com>
2024-08-31 21:37:33 +0900

    adicionando componente de login e alteracoes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PatientComponent } from './pages/patient/patient.component';
-import { ScheduleComponent } from './pages/schedule/schedule.component';
 import { SettingsComponent } from './pages/settings/settings.component';
-import { ContactComponent } from './pages/contact/contact.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo:'login', pathMatch: 'full'},
+    { path: 'login', component: LoginComponent},
     { path: 'header', component: HeaderComponent},
     { path: 'footer', component: FooterComponent},
-    { path: '', component: HomeComponent},
-    { path: 'home', component: HomeComponent},
+    { path: 'home', component: HomeComponent}, 
     { path: 'patient', component: PatientComponent},
-    { path: 'schedule', component: ScheduleComponent},
-    { path: 'settings', component: SettingsComponent},
-    { path: 'contact', component: ContactComponent},
+    { path: 'settings', component: SettingsComponent}    
 ];
